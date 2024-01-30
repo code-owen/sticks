98a97498639e355b2e3655e64653e56de305db1a
code-owen <owen1125@gmail.com>
2024-01-30 14:52:14 -0500

    Initial commit

---
+++ Sticks/Sticks.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sticks.Web.Data;
+using Sticks.Web.Models.Domain;
+using Sticks.Web.Models.ViewModels;
+
+namespace Sticks.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly SticksDbContext sticksDbContext;
+
+        public AdminTagsController(SticksDbContext sticksDbContext)
+        {
+            this.sticksDbContext = sticksDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            sticksDbContext.Tags.Add(tag);
+            sticksDbContext.SaveChanges();
+
+            return View("Ädd");
+        }
+    }
+}
---
+++ Sticks/Sticks.Web/Data/SticksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sticks.Web.Models.Domain;
+
+namespace Sticks.Web.Data
+{
+    public class SticksDbContext : DbContext
+    {
+        public SticksDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
