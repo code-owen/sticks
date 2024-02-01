528949248d041d7a5008ea192cf03a3238f5a351
code-owen <owen1125@gmail.com>
2024-02-01 14:32:46 -0500

    Tags update and delete

--- Sticks/Sticks.Web/Controllers/AdminTagsController.cs
+++ Sticks/Sticks.Web/Controllers/AdminTagsController.cs
@@ -24,6 +24,8 @@ namespace Sticks.Web.Controllers
         [HttpPost]
         [ActionName("Add")]
         public IActionResult Add(AddTagRequest addTagRequest)
+
+
         {
             //Mapping AddTagRequest to Tag domain model
             var tag = new Tag
@@ -35,7 +37,87 @@ namespace Sticks.Web.Controllers
             sticksDbContext.Tags.Add(tag);
             sticksDbContext.SaveChanges();
 
-            return View("Ädd");
+            return RedirectToAction("List");
+        }
+
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            //use dbContext to read the tags
+            var tags = sticksDbContext.Tags.ToList();
+
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = sticksDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var existingTag = sticksDbContext.Tags.Find(tag.Id);
+
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                //save changes
+                sticksDbContext.SaveChanges();
+
+                //show success notification
+                return RedirectToAction("Edit", new { id = editTagRequest.Id }); ;
+            }
+
+            //show error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = sticksDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                sticksDbContext.Tags.Remove(tag);
+                sticksDbContext.SaveChanges();
+
+                //show a success notification
+                return RedirectToAction("List");
+            }
+
+            //show an error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id }); ;
         }
     }
 }
--- Sticks/Sticks.Web/obj/Debug/net8.0/Sticks.Web.AssemblyInfo.cs
+++ Sticks/Sticks.Web/obj/Debug/net8.0/Sticks.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sticks.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457d002fbb6ff0c956d6de1ac93218660c217bdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sticks.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sticks.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
